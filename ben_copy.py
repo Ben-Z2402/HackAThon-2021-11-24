70388361b9d357948e149c4c2374df7a5f8a38b1
Ben-Z2402 <78487404+Ben-Z2402@users.noreply.github.com>
2021-11-27 15:22:20 -0500

    Update ben_copy.py

---
+++ ben_copy.py
@@ -0,0 +1,16 @@
+class main():
+    def __init__(self, savings: int):
+
+        self.savings = savings 
+
+    def user_savings(self):
+        user_savings = int(input('How much in savings?: '))
+
+    def run_program(self):
+        pass
+
+if __name__ == '__main__':
+    program = main()
+
+    while True:
+        program.run_program()
